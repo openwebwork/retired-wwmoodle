c9fdc309b46062cd3f1d9da0e6ec00939d95dfe7
gage <gage@c0722133-6baf-4dd8-8699-98d999cd4f06>
2006-06-29 16:12:01 +0000

    Removed update code since I doubt that it will help in converting from wwmoodle to wwassignment.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/wwmoodle@4188 c0722133-6baf-4dd8-8699-98d999cd4f06

--- wwassignment/db/mysql.php
+++ wwassignment/db/mysql.php
@@ -6,17 +6,17 @@ function wwmoodleassignment_upgrade($oldversion) {
 
 	global $CFG;
 
-	if ($oldversion < 2006042700) {
+	if ($oldversion < 20060827) {
 		// fix to handle course shortnames with spaces properly.
-		table_column('wwassignment_bridge', null, 'coursename', 'varchar', 15, '', '', 'not null', 'course');
-		// now properly update the table to have modified shortName
-		$aRecords = get_records('wwassignment_bridge');
-		foreach( $aRecords as $oRecord ) {
-			// get the short name:
-			$oCourse = get_record('course', 'id', $oRecord->course);
-			$oRecord->coursename = preg_replace("/[^a-zA-Z0-9]/", "", $oCourse->shortname);
-			update_record('wwassignment_bridge', $oRecord);
-		}
+/* 		table_column('wwassignment_bridge', null, 'coursename', 'varchar', 15, '', '', 'not null', 'course'); */
+/* 		// now properly update the table to have modified shortName */
+/* 		$aRecords = get_records('wwassignment_bridge'); */
+/* 		foreach( $aRecords as $oRecord ) { */
+/* 			// get the short name: */
+/* 			$oCourse = get_record('course', 'id', $oRecord->course); */
+/* 			$oRecord->coursename = preg_replace("/[^a-zA-Z0-9]/", "", $oCourse->shortname); */
+/* 			update_record('wwassignment_bridge', $oRecord); */
+/* 		} */
 	}
 	return true;
 }
