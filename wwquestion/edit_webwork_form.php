bcb145f872c01b6ab7eb88d71c2904734fef633a
mleventi <mleventi@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-07-11 20:40:12 +0000

    Clean Up.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/wwmoodle@5149 c0722133-6baf-4dd8-8699-98d999cd4f06

--- wwquestion/edit_webwork_form.php
+++ wwquestion/edit_webwork_form.php
@@ -2,16 +2,16 @@
 /**
  * The editing form code for this question type.
  *
- * @copyright &copy; 2006 YOURNAME
- * @author YOUREMAILADDRESS
+ * @copyright &copy; 2007 Matthew Leventi
+ * @author mleventi@gmail.com
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
- * @package YOURPACKAGENAME
+ * @package webwork_qtype
  *//** */
 
 require_once($CFG->dirroot.'/question/type/edit_question_form.php');
 
 /**
- * QTYPENAME editing form definition.
+ * webwork editing form definition.
  * 
  * See http://docs.moodle.org/en/Development:lib/formslib.php for information
  * about the Moodle forms library, which is based on the HTML Quickform PEAR library.
@@ -36,39 +36,14 @@ class question_edit_webwork_form extends question_edit_form {
         $mform->setHelpButton('seed', array('webwork', get_string('edit_seed', 'qtype_webwork'), 'webwork'));
         $mform->setDefault('seed', 0);
         $mform->addRule('seed', null, 'required', null, 'client');
-        
-        
-        
-        // TODO, add any form fields you need.
-        // $mform->addElement( ... );
     }
 
     function set_data($question) {
-
-       // if (isset($question->options) && isset($question->options->answers)) {
-        //    var_dump($question);
-         //   die();
-        //    $answer = reset($question->options->answers);
-        //    $question->feedback = $answer->feedback;
-        //}
-        //$question->penalty = 0;
-        // TODO, preprocess the question definition so the data is ready to load into the form.
-        // You may not need this method at all, in which case you can delete it.
-
-        // For example:
-        // if (!empty($question->options)) {
-        //     $question->customfield = $question->options->customfield;
-        // }
         parent::set_data($question);
     }
 
     function validation($data) {
         $errors = array();
-        // TODO, do extra validation on the data that came back from the form. E.g.
-        // if (/* Some test on $data['customfield']*/) {
-        //     $errors['customfield'] = get_string( ... );
-        // }
-
         if ($errors) {
             return $errors;
         } else {
