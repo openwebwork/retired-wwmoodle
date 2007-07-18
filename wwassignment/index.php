8a38fef86bd514f4d3a8b0e1c211c3562533cc69
mleventi <mleventi@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-07-18 19:25:24 +0000

    Added Description in Index Listing
    git-svn-id: http://svn.webwork.maa.org/system/trunk/wwmoodle@5187 c0722133-6baf-4dd8-8699-98d999cd4f06

--- wwassignment/index.php
+++ wwassignment/index.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: index.php,v 1.6 2007-07-18 17:58:06 mleventi Exp $
+// $Id: index.php,v 1.7 2007-07-18 19:25:24 mleventi Exp $
 
 
 /// This page lists all the instances of wwassignment in a particular course
@@ -43,19 +43,20 @@
     $strname  = get_string("name");
     $strweek  = get_string("week");
     $strtopic  = get_string("topic");
+    $strdescription = get_string('description');
     
     $strOpenDate = get_string("open_date", "wwassignment");
     $strDueDate = get_string("due_date", "wwassignment");
     
     if ($course->format == "weeks") {
-        $table->head  = array ($strweek, $strname, $strOpenDate, $strDueDate);
-        $table->align = array ("center", "left", "left", "left");
+        $table->head  = array ($strweek, $strname,$strdescription, $strOpenDate, $strDueDate);
+        $table->align = array ("center", "left", "left", "left", "left");
     } else if ($course->format == "topics") {
-        $table->head  = array ($strtopic, $strname, $strOpenDate, $strDueDate);
-        $table->align = array ("center", "left", "left", "left", "left", "left");
+        $table->head  = array ($strtopic, $strname,$strdescription, $strOpenDate, $strDueDate);
+        $table->align = array ("center", "left", "left", "left", "left", "left", "left");
     } else {
-        $table->head  = array ($strname, $strOpenDate, $strDueDate);
-        $table->align = array ("left", "left", "left", "left", "left");
+        $table->head  = array ($strname,$strdescription, $strOpenDate, $strDueDate);
+        $table->align = array ("left", "left", "left", "left", "left", "left");
     }
     $webworkclient =& new webwork_client();
     $webworkcourse = _wwassignment_mapped_course($COURSE->id,false);
@@ -64,6 +65,7 @@
         if(isset($wwassignment)) {
             //var_dump($wwassignment);
             $wwassignmentsetinfo = $webworkclient->get_assignment_data($webworkcourse,$wwassignment->webwork_set,false);
+            $description = $wwassignment->description;
             if (!$wwassignment->visible) {
                 //Show dimmed if the mod is hidden
                 $link = "<a class=\"dimmed\" href=\"view.php?id=$wwassignment->coursemodule\">$wwassignment->name</a>";
@@ -72,9 +74,9 @@
                 $link = "<a href=\"view.php?id=$wwassignment->coursemodule\">$wwassignment->name</a>";
             }
             if ($course->format == "weeks" or $course->format == "topics") {
-                $table->data[] = array ($wwassignment->section, $link, strftime("%c", $wwassignmentsetinfo['open_date']), strftime("%c", $wwassignmentsetinfo['due_date']));
+                $table->data[] = array ($wwassignment->section,  $link, $description, strftime("%c", $wwassignmentsetinfo['open_date']), strftime("%c", $wwassignmentsetinfo['due_date']));
             } else {
-                $table->data[] = array ($link, strftime("%c", $wwassignmentsetinfo['open_date']), strftime("%c", $wwassignmentsetinfo['due_date']));
+                $table->data[] = array ($link, $description, strftime("%c", $wwassignmentsetinfo['open_date']), strftime("%c", $wwassignmentsetinfo['due_date']));
             }
         }
     }
