f301f8244ec06e6ee0ad2ba4a4b599b6e918b550
mleventi <mleventi@c0722133-6baf-4dd8-8699-98d999cd4f06>
2007-09-09 18:32:03 +0000

    New configuration file.
    git-svn-id: http://svn.webwork.maa.org/system/trunk/wwmoodle@5478 c0722133-6baf-4dd8-8699-98d999cd4f06

---
+++ wwquestion/config.php
@@ -0,0 +1,13 @@
+<?php
+
+//Path to the WSDL file on the Webwork Server
+define('WWQUESTION_WSDL','http://128.151.231.20/problemserver_files/WSDL.wsdl');
+
+//Leave the rest alone
+define('WWQUESTION_PREFIX','/wwquestions');
+define('WWQUESTION_ROOTDIR','/' . SITEID . WWQUESTION_PREFIX);
+
+define('WWQUESTION_RESPONSE_TIMEOUT',120);
+
+
+?>
