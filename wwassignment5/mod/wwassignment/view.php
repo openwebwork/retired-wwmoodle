2c4ec4db5ea4de6bc0afa62b96e408c0205ed81b
Michael Gage <gage@math.rochester.edu>
2015-02-21 22:30:26 -0500

    Commit update for view.php to conform to moodle 2.8

--- wwassignment5/mod/wwassignment/view.php
+++ wwassignment5/mod/wwassignment/view.php
@@ -15,6 +15,9 @@ require_once("locallib.php");
 $id = optional_param('id', 0, PARAM_INT); // Course Module ID, or
 $a  = optional_param('a', 0, PARAM_INT);  // NEWMODULE ID
 
+$cm = get_coursemodule_from_id('wwassignment', $id, 0, false, MUST_EXIST);
+$course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+
 if($id) {
     if (! $cm = $DB->get_record("course_modules", array( "id"=>$id ))) {
         print_error("Course Module ID was incorrect");
@@ -49,11 +52,26 @@ if($USER->username == 'guest') {  # this allows guests to view webwork (signed i
 //force login
 $courseid = $course->id;
 $wwassignmentid = $wwassignment->id;
-require_login($courseid);
+require_login($courseid );
 
-// set page url
+// set page values
+
+$strwwassignments = get_string("modulenameplural", "wwassignment");
+$strwwassignment  = get_string("modulename", "wwassignment");
+
 $PAGE->set_url('/mod/wwassignment/view.php',array('id'=>$cm->id));
 $PAGE->set_pagelayout('login');
+$PAGE->set_heading("$course->fullname");
+$PAGE->set_title("$course->shortname: $wwassignment->name");
+$PAGE->set_cacheable(true);
+$PAGE->set_focuscontrol("");
+$PAGE->set_button(update_module_button($cm->id, $course->id, $strwwassignment));
+//$PAGE->navbar->add($strwwassignments,"index.php?id=$course->id");
+//$PAGE->navbar->add($wwassignment->name);
+
+$context = context_module::instance( $cm->id); // should this be module context or course context?
+$PAGE->set_context($context);
+$PAGE->set_cm($cm);
 
 //webwork code
 $wwcoursename = _wwassignment_mapped_course($courseid,false);
@@ -65,32 +83,21 @@ _wwassignment_mapcreate_user_set($wwcoursename,$wwusername,$wwsetname);
 $wwkey = _wwassignment_login_user($wwcoursename,$wwusername);
 $wwsetlink = _wwassignment_link_to_set_auto_login($wwcoursename,$wwsetname,$wwusername,$wwkey);
 
-add_to_log($course->id, "wwassignment", "view", "view.php?id=$cm->id", "$wwassignmentid",_wwassignment_cmid());
+//add_to_log($course->id, "wwassignment", "view", "view.php?id=$cm->id", "$wwassignmentid",_wwassignment_cmid());
+
+$event = \mod_wwassignment\event\course_module_viewed::create(array(
+    'objectid' => $PAGE->cm->instance,
+    'context'  => $PAGE->context,
+));
+$event->add_record_snapshot('course', $PAGE->course);
+//$event->add_record_snapshot($PAGE->cm->modname, $activityrecord);
+$event->trigger();
 
 /// Print the page header
 
-#if ($course->category) {
-#    $navigation = "<a href=\"../../course/view.php?id=$course->id\">$course->shortname</a> ->";
-#}
-
-$strwwassignments = get_string("modulenameplural", "wwassignment");
-$strwwassignment  = get_string("modulename", "wwassignment");
-
-# print_header("$course->shortname: $wwassignment->name", "$course->fullname", "$navigation <a href='index.php?id=$course->id'>$strwwassignments</a> -> $wwassignment->name", "", "", true, update_module_button($cm->id, $course->id, $strwwassignment), navmenu($course, $cm));
-
-$PAGE->set_heading("$course->fullname");
-$PAGE->set_title("$course->shortname: $wwassignment->name");
-$PAGE->set_cacheable(true);
-$PAGE->set_focuscontrol("");
-$PAGE->set_button(update_module_button($cm->id, $course->id, $strwwassignment));
-$PAGE->navbar->add($strwwassignments,"index.php?id=$course->id");
-$PAGE->navbar->add($wwassignment->name);
 echo $OUTPUT->header();
 
-
 /// Print the main part of the page
-
-
 
 // Print webwork in iframe and link to escape to have webwork in a single window
 print("<p style='font-size: smaller; color: #aaa;'>" . get_string("iframeNoShow-1", "wwassignment")
@@ -102,8 +109,8 @@ print("<iframe id='wwPage' src='$wwsetlink' frameborder='1' "
       . "height='".$CFG->wwassignment_iframeheight."'>"
       );
 
-$context = get_context_instance(CONTEXT_COURSE, $course->id);
-print("<script>ww.Init(".has_capability('moodle/course:manageactivities',$context).")</script></iframe>");
+
+print("<script>ww.Init(".has_capability('moodle/course:manageactivities',context_course::instance($course->id) ).")</script></iframe>");
 
 
 /// Finish the page
